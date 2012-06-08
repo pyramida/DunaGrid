e3e1640c10a8c682a755868cb5de484b6d3b3b7d
Petr Haljuk <petrod-git@pschds.cz>
2012-06-08 13:32:42 +0200

    Úplný základ vytvořený přes VS2010

---
+++ DunaGrid/DunaGrid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DunaGrid
+{
+    public partial class DunaGrid : Panel
+    {
+        public DunaGrid()
+        {
+            
+        }
+
+        private void DunaGrid_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DunaGridFormTest/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DunaGridFormTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dunaGrid1 = new DunaGrid.DunaGrid();
+            this.SuspendLayout();
+            // 
+            // dunaGrid1
+            // 
+            this.dunaGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dunaGrid1.Location = new System.Drawing.Point(0, 0);
+            this.dunaGrid1.Name = "dunaGrid1";
+            this.dunaGrid1.Size = new System.Drawing.Size(704, 348);
+            this.dunaGrid1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 348);
+            this.Controls.Add(this.dunaGrid1);
+            this.Name = "Form1";
+            this.Text = "DunaGrid Test Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DunaGrid.DunaGrid dunaGrid1;
+    }
+}
+
