d4ef95a4b6535df1b058ec6d98d2e5abc6b00d96
Petrod <petrod-git@pschds.cz>
2012-06-15 11:57:18 +0200

    Přidána třída FormatterCollection

---
+++ DunaGrid/formatters/FormatterCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DunaGrid.rows;
+
+namespace DunaGrid.formatters
+{
+    /// <summary>
+    /// kolekce formatteru, ktera umi vyhodnotit a vratit spravny formatter
+    /// </summary>
+    class FormatterCollection : List<IFormatter>
+    {
+        public IFormatter getMatchFormatter(IRow row)
+        {
+            foreach (IFormatter f in this)
+            {
+                if (f.isMatch(row))
+                {
+                    return f;
+                }
+            }
+
+            return null;
+        }
+    }
+}
